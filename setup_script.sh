66d834c8cfefdea76be33e0e4da41c7803fd95c2
Joseph Rance <josephhenryrance@gmail.com>
2025-05-01 21:08:05 +0100

    command fixes

--- setup_script.sh
+++ setup_script.sh
@@ -1,7 +1,8 @@
 wget https://repo.anaconda.com/archive/Anaconda3-2024.10-1-Linux-x86_64.sh
 bash Anaconda3-2024.10-1-Linux-x86_64.sh
 conda create -n main python=3.10
-pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu126 matplitlib
+pip install torch==2.6.0 torchvision==0.21.0 torchaudio==2.6.0 --index-url https://download.pytorch.org/whl/cu126
 pip install torch-scatter -f https://data.pyg.org/whl/torch-2.6.0+cu126.html
 pip install graph-transformer-pytorch
+pip install matplotlib
 git clone https://github.com/Joseph-Rance/gfn-backward-tests.git
